32d5bb86405129f910128db80662f77bca6f9d75
S0ngyuLi <lisongyu2009@gmail.com>
2015-12-06 00:11:50 -0600

    puzzle solved

--- spimbot.s
+++ spimbot.s
@@ -38,11 +38,14 @@ REQUEST_WORD = 0xffff00dc
 NODE_SIZE = 12
 
 fruit_data:  .space 260
-puzzle_grid: .space 8200
+puzzle_grid: .space 8192
+
 puzzle_word: .space 128
+
 num_rows:    .space 4
 num_cols:    .space 4
 counter:     .space 4
+flag:	     .space 4
 
 directions:
 	.word -1  0
@@ -55,10 +58,12 @@ new_node_address: .word node_memory
 # Don't put anything below this just in case they malloc more than 4096
 node_memory: .space 4096
 
+
+
 .text	
 main:	
 
-cycle:	
+
 	##li	$t4, 0
 	sw	$0, counter
 	
@@ -69,49 +74,44 @@ cycle:
 	or	$t4, $t4, OUT_OF_ENERGY_INT_MASK
 	or	$t4, $t4, 1				# global interrupt enable
 	mtc0	$t4, $12
-	
+cycle:	
 	li	$t2, 0
+	sw	$t2, flag
+	
+	li	$t2, 0		
 	sw	$t2, VELOCITY	
-	li	$t2, 90
+	li	$t2, 90		
 	sw	$t2, ANGLE	
 	li	$t2, 1 					##t2 is temporary here
 	sw	$t2, ANGLE_CONTROL
 	lw	$t0, GET_ENERGY	
 	ble	$t0, 95, we_need_energy
-	j	cycle
+	j	cycle		
 
 we_need_energy:
 	
+	
 	la	$t0, puzzle_grid
 	sw 	$t0, REQUEST_PUZZLE
+	
 	add	$a0, $t0, 8
-
+waiting_for_interrupt:
+	lw	$t0, flag
+	bne	$t0, 1, waiting_for_interrupt
+	
 	la	$a1, puzzle_word
 	li	$a2, 0
 	li	$a3, 0
 	
-	la 	$t0, puzzle_grid
-	lw 	$t1, 0($t0)
-	sw  	$t1, num_rows
-	
-	lw 	$t1, 4($t0)
-	sw 	$t1, num_cols
-	
 	jal	search_neighbors
-	
-	
 	
 	sw  	$v0, SUBMIT_SOLUTION
 	
-	move	$a0, $v0
-	li     	$v0, PRINT_INT	
-	syscall
-	
-	
 	j cycle
 	
-allocate_new_node:	
-	
+
+
+allocate_new_node:		
 	lw	$v0, new_node_address
 	add	$t0, $v0, NODE_SIZE
 	sw	$t0, new_node_address	
@@ -188,194 +188,98 @@ end:
 
 
 search_neighbors:
-	sub $sp, $sp, 40
-	sw  $s0, 0($sp)
-	sw  $s1, 4($sp)
-	sw  $s2, 8($sp)
-	sw  $s3, 12($sp)
-	sw  $s4, 16($sp)
-	sw  $s5, 20($sp)
-	sw  $s6, 24($sp)
-	sw  $s7, 28($sp)
-	
-		
-	beq $a1, 0, return_null
-	
-	li  $t0, 0                  ##t0 serves as int i
-	
-loop:	bge $t0, 4, return_null
+	bne	$a1, 0, sn_main		# !(word == NULL)
+	li	$v0, 0			# return NULL (data flow)
+	jr	$ra			# return NULL (control flow)
 
-	la  $t7, directions	
-	
-	mul $t1, $t0, 2
-	
-	mul $t1, $t1, 4
-	
-	add $t1, $t1, $t7
-	lw  $t2, 0($t1)
-	add $t2, $t2, $a2           ##t2 serves as next_row
-	
-	mul $t1, $t0, 2
-	add $t1, $t1, 1
-	
-	mul $t1, $t1, 4
-	
-	add $t1, $t1, $t7
-	
-	lw $t3, 0($t1)
-	add $t3, $t3, $a3           ##t3 serves as next_col
-	
-	lw  $s0, num_rows
-	lw  $s1, num_cols
-	
-	ble $t2, -1, end_of_loop
-	bge $t2, $s0, end_of_loop
-	ble $t3, -1, end_of_loop
-	bge $t3, $s1, end_of_loop
-	
-	mul $t4, $t2, $s1
-	add $t4, $t4, $t3
-	add $t4, $t4, $a0
-	lbu $t4, 0($t4)
-	lbu $t5, 0($a1)
-	bne $t4, $t5, end_of_loop
-	
-	##add $t5, $a1, 1
-	lb  $t5, 1($a1)
-	beq $t5, 0, jump1
-	
-	mul $t4, $t2, $s1
-	add $t4, $t4, $t3
-	add $t4, $t4, $a0
-	
-	li  $t5, 42
-	
-	sb  $t5, 0($t4)
-	
-	## I'll use $t6 as my next_node
-	move $s2, $a0
-	move $s3, $a1
-	move $s4, $a2
-	move $s5, $a3 
-	move $s6, $ra
-	move $s7, $t0 ##i
-	sw   $t2, 32($sp)
-	sw   $t3, 36($sp)
-	
-	add  $a1, $a1, 1
-	move $a2, $t2
-	move $a3, $t3
-	jal  search_neighbors
-	
-	move $t6, $v0
-	
-	move $a0, $s2
-	move $a1, $s3
-	move $a2, $s4
-	move $a3, $s5 
-	move $ra, $s6
-	move $t0, $s7
-	lw   $t2, 32($sp)
-	lw   $t3, 36($sp)
-	
-	mul $t4, $t2, $s1
-	add $t4, $t4, $t3
-	add $t4, $t4, $a0
-	
-	lbu  $t5, 0($a1)
-	sb   $t5, 0($t4)
-	
-	beq  $t6, 0, end_of_loop
-	move $s2, $a0
-	move $s3, $a1
-	move $s4, $a2
-	move $s5, $a3 
-	move $s6, $ra
-	move $s7, $t0 ##i
-	sw   $t2, 32($sp)
-	sw   $t3, 36($sp)
-	
-	move $a0, $t2
-	move $a1, $t3
-	move $a2, $t6
-	jal  set_node
-	
-	move $ra, $s6
-	
-	lw  $s0, 0($sp)
-	lw  $s1, 4($sp)
-	lw  $s2, 8($sp)
-	lw  $s3, 12($sp)
-	lw  $s4, 16($sp)
-	lw  $s5, 20($sp)
-	lw  $s6, 24($sp)
-	lw  $s7, 28($sp)
-	
-	add  $sp, $sp, 40
-	
-	jr   $ra
-	
-	
-jump1:	
-	move $s2, $a0
-	move $s3, $a1
-	move $s4, $a2
-	move $s5, $a3 
-	move $s6, $ra
-	move $s7, $t0 ##i
-	sw   $t2, 32($sp)
-	sw   $t3, 36($sp)
-	
-	move $a0, $t2
-	move $a1, $t3
-	li   $a2, 0
-	
-	
-	jal  set_node
+sn_main:
+	sub	$sp, $sp, 36
+	sw	$ra, 0($sp)
+	sw	$s0, 4($sp)
+	sw	$s1, 8($sp)
+	sw	$s2, 12($sp)
+	sw	$s3, 16($sp)
+	sw	$s4, 20($sp)
+	sw	$s5, 24($sp)
+	sw	$s6, 28($sp)
+	sw	$s7, 32($sp)
 
+	move	$s0, $a0		# puzzle
+	move	$s1, $a1		# word
+	move	$s2, $a2		# row
+	move	$s3, $a3		# col
+	li	$s4, 0			# i
+
+sn_loop:
+	mul	$t0, $s4, 8		# i * 8
+	lw	$t1, directions($t0)	# directions[i][0]
+	add	$s5, $s2, $t1		# next_row
+	lw	$t1, directions+4($t0)	# directions[i][1]
+	add	$s6, $s3, $t1		# next_col
+
+	ble	$s5, -1, sn_next	# !(next_row > -1)
+	lw	$t0, num_rows
+	bge	$s5, $t0, sn_next	# !(next_row < num_rows)
+	ble	$s6, -1, sn_next	# !(next_col > -1)
+	lw	$t0, num_cols
+	bge	$s6, $t0, sn_next	# !(next_col < num_cols)
+
+	mul	$t0, $s5, $t0		# next_row * num_cols
+	add	$t0, $t0, $s6		# next_row * num_cols + next_col
+	add	$s7, $s0, $t0		# &puzzle[next_row * num_cols + next_col]
+	lb	$t0, 0($s7)		# puzzle[next_row * num_cols + next_col]
+	lb	$t1, 0($s1)		# *word
+	bne	$t0, $t1, sn_next	# !(puzzle[next_row * num_cols + next_col] == *word)
+
+	lb	$t0, 1($s1)		# *(word + 1)
+	bne	$t0, 0, sn_search	# !(*(word + 1) == '\0')
+	move	$a0, $s5		# next_row
+	move	$a1, $s6		# next_col
+	li	$a2, 0			# NULL
+	jal	set_node		# $v0 will contain return value
+	j	sn_return
+
+sn_search:
+	li	$t0, '*'
+	sb	$t0, 0($s7)		# puzzle[next_row * num_cols + next_col] = '*'
+	move	$a0, $s0		# puzzle
+	add	$a1, $s1, 1		# word + 1
+	move	$a2, $s5		# next_row
+	move	$a3, $s6		# next_col
+	jal	search_neighbors
+	lb	$t0, 0($s1)		# *word
+	sb	$t0, 0($s7)		# puzzle[next_row * num_cols + next_col] = *word
+	beq	$v0, 0, sn_next		# !next_node
+	move	$a0, $s5		# next_row
+	move	$a1, $s6		# next_col
+	move	$a2, $v0		# next_node
+	jal	set_node
+	j	sn_return
+
+sn_next:
+	add	$s4, $s4, 1		# i++
+	blt	$s4, 4, sn_loop		# i < 4
 	
-	
-	move $ra, $s6
-	
-	lw  $s0, 0($sp)
-	lw  $s1, 4($sp)
-	lw  $s2, 8($sp)
-	lw  $s3, 12($sp)
-	lw  $s4, 16($sp)
-	lw  $s5, 20($sp)
-	lw  $s6, 24($sp)
-	lw  $s7, 28($sp)
-	
-	add $sp, $sp, 40
-	jr   $ra
-	
-	
-end_of_loop:
-	add $t0, $t0, 1
-	j   loop
-	
-return_null:
-	
-	
-	li	$v0, 0	
-	
-	lw  $s0, 0($sp)
-	lw  $s1, 4($sp)
-	lw  $s2, 8($sp)
-	lw  $s3, 12($sp)
-	lw  $s4, 16($sp)
-	lw  $s5, 20($sp)
-	lw  $s6, 24($sp)
-	lw  $s7, 28($sp)
-	
-	add     $sp, $sp, 40		# Don't forget to replace this!
+	li	$v0, 0			# return NULL (data flow)
+
+sn_return:
+	lw	$ra, 0($sp)
+	lw	$s0, 4($sp)
+	lw	$s1, 8($sp)
+	lw	$s2, 12($sp)
+	lw	$s3, 16($sp)
+	lw	$s4, 20($sp)
+	lw	$s5, 24($sp)
+	lw	$s6, 28($sp)
+	lw	$s7, 32($sp)
+	add	$sp, $sp, 36
 	jr	$ra
 ##search neighbour finished
 
 
 
 .kdata				# interrupt handler data (separated just for readability)
-chunkIH:	.space 16	# space for 4 registers
+chunkIH:	.space 20	# space for 4 registers
 non_intrpt_str:	.asciiz "Non-interrupt exception\n"
 unhandled_str:	.asciiz "Unhandled interrupt type\n"
 
@@ -389,6 +293,7 @@ interrupt_handler:
 	sw	$a1, 4($k0)		# by storing them to a global variable     
 	sw	$t0, 8($k0)
 	sw	$t1, 12($k0)
+	sw	$v0, 16($k0)
 	
 	mfc0	$k0, $13		# Get Cause register                       
 	srl	$a0, $k0, 2                
@@ -474,12 +379,22 @@ no_energy:
 	j 	interrupt_dispatch 
 					
 puzzle_interrupt:			
-									
-	la	$t0, puzzle_word	
-	sw	$t0, 0xffff00dc($0)	
 					
 	sw	$a1, REQUEST_PUZZLE_ACK # acknowledge interrupt
 	
+	li	$t0, 1
+	sw	$t0, flag
+	
+	la	$t0, puzzle_word
+	sw	$t0, REQUEST_WORD
+	
+	
+	la 	$t0, puzzle_grid
+	lw 	$t1, 0($t0)
+	sw  	$t1, num_rows
+	
+	lw 	$t1, 4($t0)
+	sw 	$t1, num_cols
 	
 	j 	interrupt_dispatch	
 					
@@ -495,6 +410,7 @@ done:
 	lw	$a1, 4($k0)
 	lw	$t0, 8($k0)
 	lw	$t1, 12($k0)
+	lw	$v0, 16($k0)
 .set noat
 	move	$at, $k1		# Restore $at
 .set at 
